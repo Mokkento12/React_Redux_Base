203ed077950f97593ab3150367304a96b8d61415
roman alekseev <roma.alekseev.90@bk.ru>
2025-02-20 10:31:07 +0300

    Добавлен компонент Comments

---
+++ src/Comments.js
@@ -0,0 +1,18 @@
+import SingleComment from "./SingleComment";
+
+function Comments(props) {
+  console.log("comments>>>", props);
+
+  return (
+    <div className="card-comments">
+      <form className="comments-item-create">
+        <input type="text" />
+        <input type="submit" hidden />
+      </form>
+
+      <SingleComment />
+    </div>
+  );
+}
+
+export default Comments;
